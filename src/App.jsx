675164cd93e00fc6ab3aa5b724a5f0a535cc9e51
Kubuses <kubuses11@gmail.com>
2024-01-27 16:42:25 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState('')
+
+  return (
+    <>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="name-display">
+        {name && <p>Hello, {name}!</p>}
+      </div>
+    </>
+  )
+}
+
+export default App
